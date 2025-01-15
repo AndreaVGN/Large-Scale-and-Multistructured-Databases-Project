6f6ea86c76c6715720fa437dfeba3298f3cf8f37
franc <francesco.galardi.fg@gmail.com>
2025-01-15 14:18:30 +0100

    v4

--- src/main/java/com/example/WanderHub/demo/model/Accomodation.java
+++ src/main/java/com/example/WanderHub/demo/model/Accomodation.java
@@ -1,4 +1,13 @@
 package com.example.WanderHub.demo.model;
 
 public class Accomodation {
+    private int accomodationId;
+    private String description;
+    private String type;
+    private boolean[] facilities;
+    public String place;
+    public String address;
+    public String[] dates;
+    public int maxGuestSize;
+
 }
