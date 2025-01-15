7e79eedc0ec083b1bd21846a6be644b84d67266b
franc <francesco.galardi.fg@gmail.com>
2025-01-15 14:30:02 +0100

    v4

--- src/main/java/com/example/WanderHub/demo/model/Accomodation.java
+++ src/main/java/com/example/WanderHub/demo/model/Accomodation.java
@@ -5,9 +5,53 @@ public class Accomodation {
     private String description;
     private String type;
     private boolean[] facilities;
-    public String place;
-    public String address;
-    public String[] dates;
-    public int maxGuestSize;
+    private String place;
+    private String address;
+    private String[] dates;
+    private int maxGuestSize;
+    private int costPerNight;
+    private double averageRate;
+    private String[] photos;
+
+    public int getAccomodationId() {return accomodationId;}
+    public String getDescription() {return description;}
+    public String getType() {return type;}
+    public boolean[] getFacilities() {return facilities;}
+    public String getPlace() {return place;}
+    public String getAddress() {return address;}
+    public String[] getDates() {return dates;}
+    public int getMaxGuestSize() {return maxGuestSize;}
+    public int getCostPerNight() {return costPerNight;}
+    public double getAverageRate() {return averageRate;}
+    public String[] getPhotos() {return photos;}
+
+    public void setAccomodationId(int accomodationId) {this.accomodationId = accomodationId;}
+    public void setDescription(String description) {this.description = description;}
+    public void setType(String type) {this.type = type;}
+    public void setFacilities(boolean[] facilities) {this.facilities = facilities;}
+    public void setPlace(String place) {this.place = place;}
+    public void setAddress(String address) {this.address = address;}
+    public void setDates(String[] dates) {this.dates = dates;}
+    public void setMaxGuestSize(int maxGuestSize) {this.maxGuestSize = maxGuestSize;}
+    public void setCostPerNight(int costPerNight) {this.costPerNight = costPerNight;}
+    public void setAverageRate(double averageRate) {this.averageRate = averageRate;}
+    public void setPhotos(String[] photos) {this.photos = photos;}
+
+    @Override
+    public String toString() {
+        return "Accomodation{" +
+                "accomodationId=" + accomodationId +
+                ", description='" + description + '\'' +
+                ", type='" + type + '\'' +
+                ", facilities=" + java.util.Arrays.toString(facilities) +
+                ", place='" + place + '\'' +
+                ", address='" + address + '\'' +
+                ", dates=" + java.util.Arrays.toString(dates) +
+                ", maxGuestSize=" + maxGuestSize +
+                ", costPerNight=" + costPerNight +
+                ", averageRate=" + averageRate +
+                ", photos=" + java.util.Arrays.toString(photos) +
+                '}';
+    }
 
 }
