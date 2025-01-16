5e1cc55db1dbabebc956629828af1d86cbb9d9a8
AndreaVGN <andreavagnoli2002@gmail.com>
2025-01-16 15:02:35 +0100

    v2

--- src/main/java/com/example/WanderHub/demo/model/Review.java
+++ src/main/java/com/example/WanderHub/demo/model/Review.java
@@ -1,18 +1,43 @@
 package com.example.WanderHub.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class Review {
     private int reviewId;
     private String reviewText;
     private double rating;
+
+    // Getter e Setter
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    // Metodo toString
+    @Override
+    public String toString() {
+        return "Review{" +
+                "reviewId=" + reviewId +
+                ", reviewText='" + reviewText + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 }
+
