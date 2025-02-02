5a26a55305805d108005f4f8f3a7bfadc93ffbae
AndreaVGN <andreavagnoli2002@gmail.com>
2025-02-02 18:26:27 +0100

    v2

--- src/main/java/com/example/WanderHub/demo/config/RedisHttpSessionConfig.java
+++ src/main/java/com/example/WanderHub/demo/config/RedisHttpSessionConfig.java
@@ -1,4 +1,4 @@
-package com.example.WanderHub.demo.config;
+/*package com.example.WanderHub.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,3 +16,4 @@ public class RedisHttpSessionConfig {
         return new JedisConnectionFactory();
     }
 }
+*/
