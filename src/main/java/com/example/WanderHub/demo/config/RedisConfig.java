09e7c5527a0640eca5b5d6aa6f7fa47be695667e
AndreaVGN <andreavagnoli2002@gmail.com>
2025-02-11 09:36:11 +0100

    ciao

--- src/main/java/com/example/WanderHub/demo/config/RedisConfig.java
+++ src/main/java/com/example/WanderHub/demo/config/RedisConfig.java
@@ -3,19 +3,33 @@ package com.example.WanderHub.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Configura Redis Sentinel con Lettuce
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master("mymaster")  // Nome del master definito in Redis Sentinel
+                .sentinel("10.1.1.9", 26379)  // Indirizzo e porta di uno dei Sentinel
+                .sentinel("10.1.1.71", 26379) // Indirizzo e porta di un altro Sentinel
+                .sentinel("10.1.1.74", 26379); // Terzo Sentinel, se lo hai
+
+        return new LettuceConnectionFactory(sentinelConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
-
